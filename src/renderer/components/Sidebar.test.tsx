545226707a6b113f3c500f87494a3ce614a3d294
Bernal I. Hernández Barquero <bhbarquero@outlook.com>
2025-11-23 20:26:26 -0600

    test: Add unit tests for Player, Sidebar, and SongList components.

--- src/renderer/components/Sidebar.test.tsx
+++ src/renderer/components/Sidebar.test.tsx
@@ -3,20 +3,20 @@ import { Sidebar } from './Sidebar';
 import '@testing-library/jest-dom';
 
 describe('Sidebar', () => {
-    it('renders sidebar title', () => {
-        render(<Sidebar />);
-        expect(screen.getByText('Uriyyá Music Player')).toBeInTheDocument();
-    });
+  it('renders sidebar title', () => {
+    render(<Sidebar />);
+    expect(screen.getByText('Uriyyá Music Player')).toBeInTheDocument();
+  });
 
-    it('renders navigation items', () => {
-        render(<Sidebar />);
-        expect(screen.getByText('Recently Added')).toBeInTheDocument();
-        expect(screen.getByText('Albums')).toBeInTheDocument();
-        expect(screen.getByText('Artists')).toBeInTheDocument();
-    });
+  it('renders navigation items', () => {
+    render(<Sidebar />);
+    expect(screen.getByText('Recently Added')).toBeInTheDocument();
+    expect(screen.getByText('Albums')).toBeInTheDocument();
+    expect(screen.getByText('Artists')).toBeInTheDocument();
+  });
 
-    it('renders section title', () => {
-        render(<Sidebar />);
-        expect(screen.getByText('Your Playlists')).toBeInTheDocument();
-    });
+  it('renders section title', () => {
+    render(<Sidebar />);
+    expect(screen.getByText('Your Playlists')).toBeInTheDocument();
+  });
 });
