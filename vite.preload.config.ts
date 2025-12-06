c3c12caaa05f66cd9800cb86565e42398c699810
Bernal I. Hernández Barquero <bhbarquero@outlook.com>
2025-12-06 11:13:11 -0600

    chore: remove server watch configuration from Vite config files

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,10 +1,4 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({
-    server: {
-        watch: {
-            ignored: ['**/coverage/**'],
-        },
-    },
-});
+export default defineConfig({});
