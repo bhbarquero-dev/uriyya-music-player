76dadfdfce0d8fe4064a7e54f426c6440abd5a1b
Bernal I. Hernández Barquero <bhbarquero@outlook.com>
2025-12-06 11:05:38 -0600

    chore: remove unnecessary build watch configuration and clean up ignored paths in renderer config

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
-    build: {
-        watch: {
-            include: ['src/main/**'],
-        },
-    },
     server: {
         watch: {
             ignored: ['**/coverage/**'],
